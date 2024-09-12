b2f09c50ed92b9349432b8ac6cb12ed2847e2a0e
Strahinja Gavrilović <79333314+strajag@users.noreply.github.com>
2024-09-12 14:40:18 +0200

    Add files via upload

---
+++ src/com/straykovsky/ball_and_mouse/Ball.java
@@ -0,0 +1,13 @@
+package com.straykovsky.ball_and_mouse;
+
+public class Ball {
+
+    double x;
+    double y;
+    double size;
+
+    double speed;
+
+    double vectorX;
+    double vectorY;
+}
---
+++ src/com/straykovsky/ball_and_mouse/Main.java
@@ -0,0 +1,9 @@
+package com.straykovsky.ball_and_mouse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Frame();
+    }
+}
+
